586050e64673bfffb70f4e0cf901e609738d5d30
Filip Sannholm <filip.sannholm@folksam.fi>
2017-02-20 15:24:40 +0200

    Added env handling for issue-fetcher

--- issue-fetcher/index.js
+++ issue-fetcher/index.js
@@ -2,6 +2,9 @@ const request = require('request');
 const util = require('util');
 const fs = require('fs');
 const params = require('../params');
+const dotenv = require('dotenv');
+
+dotenv.load()
 
 function getNewSession(params){
 	return new Promise(function(resolve, reject){
@@ -57,21 +60,39 @@ function getFilterTasks(session, filter){
 		});
 	});
 }
-getNewSession(params).then(function(sessionHeader, params){
-	getFilterMeta(sessionHeader).then(function(url){
-		getFilterTasks(sessionHeader, url).then(function(body){
-			console.dir(body, {depth: null});
-			console.log(body.issues.length);
-			fs.writeFile("./mocks/issues.json", JSON.stringify(body));
-		}, 
+
+if(process.env.REACT_APP_ENVIRONMENT === 'live'){
+	getNewSession(params).then(function(sessionHeader, params){
+		getFilterMeta(sessionHeader).then(function(url){
+			getFilterTasks(sessionHeader, url).then(function(body){
+				console.dir(body, {depth: null});
+				console.log(body.issues.length);
+				fs.writeFile("./mocks/issues.json", JSON.stringify(body));
+				return body;
+			}, 
+			function(err){
+				console.log(err);
+			})
+		},
 		function(err){
-			console.log(err);
-		})
+			console.log(err)
+		});
 	},
 	function(err){
-		console.log(err)
+		console.log(err);
 	});
-},
-function(err){
-	console.log(err);
-});
+} else if(process.env.REACT_APP_ENVIRONMENT === 'offline'){
+	console.log('offline mode...');
+	try{
+		let fd = fs.openSync('./mocks/issues.json', 'r');
+		let issues = fs.readFile('./mocks/issues.json', 'utf8', (err, data) =>{
+			if(err) {
+				throw err;
+			} else return data;
+		})
+	} catch (e){
+		console.log(e);
+	}
+} else {
+	console.log('REACT_APP_ENVIRONMENT not set in .env!');
+}
--- src/App.js
+++ src/App.js
@@ -8,31 +8,31 @@ import SummaryAssignee from './components/SummaryAssignee';
 import './css/main.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppBar title="Jira report tool"/>
-        <Paper zDepth={1}>
-          <IssuesTable issues={this.props.issues}/>
-        </Paper>
-        <Paper zDepth={1}>
-          <SummaryAssignee summary={this.props.summary}/>
-        </Paper>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<AppBar title="Jira report tool"/>
+				<Paper zDepth={1}>
+					<IssuesTable issues={this.props.issues}/>
+				</Paper>
+				<Paper zDepth={1}>
+					<SummaryAssignee summary={this.props.summary}/>
+				</Paper>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
-  return {
-    issues : state.tickets.issues,
-    summary: state.summary
-  }
+	console.log(state);
+	return {
+		issues : state.tickets.issues,
+		summary: state.summary
+	}
 }
 
 App = connect(
-  mapStateToProps
+	mapStateToProps
 )(App)
 
 export default App;
